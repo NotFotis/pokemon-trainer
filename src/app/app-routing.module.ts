e52132f0262ba690bc054bb24dda616779ac0789
f.staikos7 <f.staikos7@gmail.com>
2023-02-25 11:49:26 +0200

    feat: Added pages,components,services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { ProfilePage } from './pages/profile/profile.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo: "/login"
+  },
+  {
+    path: "login",
+    component: LoginPage
+  },
+  {
+    path: "pokemon",
+    component: PokemonCataloguePage
+  },
+  {
+    path: "profile",
+    component: ProfilePage
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], //import a module
+  exports: [RouterModule] //expose module and its features
 })
 export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environmet ={
+    production: false,
+    apiUsers: "https://cg-pokemon-trainer-api-production.up.railway.app/users",
+    apiPokemon: "https://pokeapi.co/api/v2/pokemon"
+};
