aaf01f325d3e9bf050bac32c4252cef88188d0bf
Christos <xrist.giann@gmail.com>
2023-03-02 10:29:13 +0200

    fix: redirecting to catalogue page when user manualy types /login or just the home url ( / ) and not at login page. Because user is already signed in.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PokemonCatalogueService } from './service/pokemon-catalogue.service';
 import { UserServiceService } from './service/user-service.service';
 import { SessionUtil } from './utils/session.util';
@@ -11,18 +12,24 @@ import { SessionUtil } from './utils/session.util';
 export class AppComponent implements OnInit{
   constructor(
     private readonly userService: UserServiceService,
-    private readonly pokemonService: PokemonCatalogueService
+    private readonly pokemonService: PokemonCatalogueService,
+    private readonly router: Router
   ){}
 
   ngOnInit(): void {
-    //checking if there are pokemons data at session storage and user exists
-    //if there are data at session, load the data from there and dont fetch the api
+    //checking if user exists
       if(this.userService.user){
+
+        //checking if there are pokemons data at session storage
+        //if there are data at session, load the data from there and dont fetch the api
         if (SessionUtil.storageRead("pokemons") === undefined){                         //There are not data at session storage,
           this.pokemonService.findAllPokemons();                                        // so take the data from api.
         }else{                                                                          //Data exists at session storage,
           this.pokemonService.pokemonsFromSession(SessionUtil.storageRead("pokemons")); //so load the data from storage
         }
+
+        //user exists so navigate the trainer at pokemon catalogue page
+        this.router.navigateByUrl("/pokemon");
       }
   }
 }
