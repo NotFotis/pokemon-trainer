78db6e48c89ec69e91c631081e7ff5238b54b7df
Fotis Staikos <64866131+NotFotis@users.noreply.github.com>
2023-03-02 16:06:37 +0200

    Delete environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment ={
-    production: false,
-    apiUsers: "https://cg-pokemon-trainer-api-production.up.railway.app/trainers",
-    apiPokemon: "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
-    apiKey: "HoBzRKFu6yfNeE4O0t3bVG8KhO4nCkdU76LY6YagVsGINxMvoNxyt1X7Zltp9JEm"
-};
